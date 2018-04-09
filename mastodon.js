78acc53e78bd64acc851bf9520819dd1a2cb90ce
Aly-ve <alysson.hussin@gmail.com>
2018-04-09 23:23:02 +0200

    Browsers management and callback
    * Add browsers management. Mastodon-Share-Button support all navigators (even IE!)
    * Need a global variable to customize modal and callbacks (to open a modal/dialog, close it, etc)

--- mastodon.js
+++ mastodon.js
@@ -9,22 +9,58 @@
   console.log('test')
   for(var i = 0; i < buttons.length ; i++) {
     (function(j) {
-      if (isFirefox()) {
-        var target = buttons[j].dataset.target;
-        var name = buttons[j].dataset.name;
-        var buttonStyle = buttons[j].dataset.buttonstyle;
-        var button = document.createElement('button');
+
+      function msbOpenModal() {
+        if (msbConfig && msbConfig.openModal && msbConfig.addressFieldId) {
+          msbConfig.openModal();
+        }
+      }
+
+      function msbCloseModal() {
+        if (msbConfig && msbConfig.closeModal) {
+          msbConfig.closeModal();
+        }
+      }
+
+      var target = buttons[j].dataset.target;
+      var name = buttons[j].dataset.name;
+      var buttonStyle = buttons[j].dataset.buttonstyle;
+      var callbackOpenModel = buttons[j].dataset.open;
+      
+      var button = document.createElement('button');
+
+      if (buttonStyle)
+        button.setAttribute('class', buttonStyle)
+
+      var buttonText = document.createTextNode('Toot');
+      button.appendChild(buttonText);
+      buttons[j].appendChild(button);
   
-        if (buttonStyle)
-          button.setAttribute('class', buttonStyle)
-  
-        var buttonText = document.createTextNode('Toot');
-        button.appendChild(buttonText);
-        buttons[j].appendChild(button);
-    
-        button.addEventListener('click', function () {
-            window.open('web+mastodon://share?text=' + name + '%20' + 'target', '__blank');
-        }, false)
+      button.addEventListener('click', function () {
+        var windowId = null;
+        if (isFirefox()) {
+          windowId = window.open('web+mastodon://share?text=' + name + '%20' + target, '__blank');
+          if (!windowId) {
+            mbsOpenModal();
+          }
+        } else {
+          msbOpenModal();
+        }
+      }, false)
+
+      if (msbConfig && msbConfig.addressFieldId && msbConfig.buttonModalId) {
+        document.getElementById(msbConfig.buttonModalId).addEventListener('click', function () {
+          var msbInstanceAddress = document.getElementById(msbConfig.addressFieldId).value;
+
+          /**
+           * @Todo: verify the url with a regular expression
+           * @Todo: add cookies management to save address instance
+           */
+          if (msbInstanceAddress && msbInstanceAddress.length > 0) {
+            window.open(msbInstanceAddress + '/share?text=' + name + '%20' + target, '__blank');
+            msbCloseModal()
+          }
+        }, false);
       }
     })(i)
   }
